8e5d9c91ba46d947358949a65e752044d0f9acd7
Ryan L Buchanan <buchanan.ryan22@gmail.com>
2020-10-29 12:30:34 -0600

    Modified routes.py

--- routes.py
+++ routes.py
@@ -1,4 +1,4 @@
-from worldbank import app
+from worldbankapp import app
 
 from flask import render_template
 
@@ -10,3 +10,15 @@ def index():
 @app.route('/project-one')
 def project_one():
     return render_template('project_one.html')
+
+@app.route('/virtual-reality')
+def virtual_reality():
+    return render_template('virtual_reality.html')
+
+@app.route('/data-dashboard')
+def data_dashboard():
+    return render_template('data_dashboard.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
