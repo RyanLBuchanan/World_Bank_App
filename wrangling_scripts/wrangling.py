62d2dcecef22182bb6beb1532671026ea75fecce
Ryan L Buchanan <buchanan.ryan22@gmail.com>
2020-10-30 13:27:39 -0600

    Stopped mid-stream to head to work

--- wrangling_scripts/wrangling.py
+++ wrangling_scripts/wrangling.py
@@ -1,8 +1,41 @@
-from worldbankapp import app
-from flask import render_template
 import pandas as pd
+import plotly.graph_objs as go
 
-df = pd.read_csv('data/API_SP.RUR.TOTL.ZS_DS2_en_csv_v2_9948275.csv', skiprows=4)
+def cleandata(dataset, keepcolumns = ['Country Name', '1990', '2015'], value_variables = ['1990', '2015']):
+    """
+    Clean World Bank data for a visualization Dashboard
+
+    Keep data range of dates in keep_columns variable and data for the top 10 economies
+    Reorient the columns into a year, country and value
+    Save the results to a csv file
+
+    Args:
+        dataset (str): name of the csv data file
+
+    Returns:
+        None
+    """
+    df = pd.read_csv('data/API_SP.RUR.TOTL.ZS_DS2_en_csv_v2_9948275.csv', skiprows=4)
+
+    # Keep only the columns of interest (years and country name)
+    df = df[keepcolumns]
+
+    top10country = ['United States', 'China', 'Japan', 'Germany', 'United Kingdom', 'India', 'France', 'Brazil', 'Italy', 'Canada']
+    df = df[df['Country Name'].isin(top10country)]
+
+    # Melt year columns and convert year to date time
+    df_melt = df.melt(id_vars='Country Name', value_vars = value_variables)
+    df_melt.columns = ['country', 'year', 'variable']
+    df_melt['year'] = df_melt['year'].astype('datetime64[ns]').dt.year
+
+    # Output clean csv file
+    return df_melt
+
+
+
+
+
+
 
 # Filter for 1990 and 2015, top 10 economies
 df = df[['Country Name', '1990', '2015']]
