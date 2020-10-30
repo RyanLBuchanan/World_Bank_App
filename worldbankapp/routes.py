512f80755a214784b67e1839d460aa6feb0f169d
Ryan L Buchanan <buchanan.ryan22@gmail.com>
2020-10-30 10:40:15 -0600

    refactor: Imported data_wrangling.py into routes.py Imported data_wrangling.py into routes.py to preserve simplicity of routes.py No footer

--- worldbankapp/routes.py
+++ worldbankapp/routes.py
@@ -3,7 +3,9 @@ from flask import render_template
 import pandas as pd
 from wrangling_scripts.wrangling import data_wrangling
 
+data = data_wrangling()
 
+print(data)
 
 @app.route('/')
 @app.route('/index')
