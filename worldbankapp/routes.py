f1c713a24024ccd72bd58669ee168a76fe28281e
Ryan L Buchanan <buchanan.ryan22@gmail.com>
2020-10-30 10:37:00 -0600

    feat: Added data_wrangling script

--- worldbankapp/routes.py
+++ worldbankapp/routes.py
@@ -1,6 +1,9 @@
 from worldbankapp import app
 from flask import render_template
 import pandas as pd
+from wrangling_scripts.wrangling import data_wrangling
+
+
 
 @app.route('/')
 @app.route('/index')
